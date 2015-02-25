796d921735e55d09171958da2780312bb5f1e241
FaizaTahir <faizatahir94@gmail.com>
2015-02-25 23:10:29 +0500

    Create Myclasses.java

---
+++ Myclasses.java
@@ -0,0 +1,23 @@
+package lab2;
+
+
+public class Myclasses {
+	int value;
+	String name;
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+
+}
